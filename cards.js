dd0638e5a64619970023b09c8c7d1ddc7dc9064e
acastilhos-hub <a.castilhos@escola.pr.gov.br>
2026-06-30 02:16:55 -0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,27 @@
+criaCartao('Mandaçaia', 
+    'O que a Mandaçaia poliniza no pomar e na horta?', 
+    'Tomate, berinjela, jiló, pimentão e pimentas.',);
+
+criaCartao(
+  'Uruçu', 
+  'O que a abelha Uruçu poliniza?', 
+  'Ela é excelente para polinizar grandes árvores nativas, além de culturas como o café, o coco e o urucum.'
+);
+
+criaCartao(
+  'Tubuna', 
+  'O que a abelha Tubuna poliniza?', 
+  'Muito ativa e ágil, ela poliniza flores de abóbora, maxixe, melancia e árvores frutíferas como a pitangueira.'
+);
+
+criaCartao(
+  'Iraí', 
+  'O que a abelha Iraí poliniza?', 
+  'Muito adaptada às cidades, ela poliniza pequenas flores de jardins, plantas medicinais e hortaliças como o manjericão e o hortelã.'
+);
+
+criaCartao(
+  'Mirim', 
+  'Qual a importância da abelha Mirim?', 
+  'Por ser minúscula, ela poliniza flores bem pequenas que outras abelhas não conseguem entrar, sendo vital para mini-orquídeas e pequenos frutos selvagens.'
+);
